10e911c97265d96b1f555f5e4bcacce00f3d60e9
piotr <aranruth94@gmail.com>
2024-07-24 15:08:21 +0200

    add eureka discovery server

---
+++ services/discovery/src/test/java/com/piotr/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piotr.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
